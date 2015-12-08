d3870d57141d3acac573818bdeab74268c2df68f
Dusan Klinec <dusan.klinec@gmail.com>
2015-12-08 16:04:41 +0100

    new build config

--- install-re-cached.sh
+++ install-re-cached.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+/bin/rm -rf "$HOME/re"
 # check to see if RE folder is empty
 if [ ! -d "$HOME/re/lib" ]; then
   /bin/rm -rf re-0.4.14.tar.gz re-0.4.14 | echo "Not deleted"
@@ -9,6 +10,9 @@ if [ ! -d "$HOME/re/lib" ]; then
   sed -i.bu "s#/usr/local#${HOME}/re#g" Makefile
   make info && make && make install
   sed -i.bu "s#LIBRE_PATH := ../re#LIBRE_PATH := $HOME/re#g" $HOME/re/share/re/re.mk
+  sed -i.bu 's#$(LIBRE_PATH)/include#$(LIBRE_PATH)/include/re#g' $HOME/re/share/re/re.mk
+  sed -i.bu 's#$(LIBRE_PATH)/libre#$(LIBRE_PATH)/lib/libre#g' $HOME/re/share/re/re.mk
+  sed -i.bu 's#echo "$(LIBRE_PATH)")#echo "$(LIBRE_PATH)/lib")#g' $HOME/re/share/re/re.mk
 else
   echo 'Using cached directory.';
 fi
